135328091ec1c64d580e048e12f1a8750a161f55
CrucheGucci <a.kazymirenko@kmds.ua>
2026-03-02 11:23:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,60 +2,63 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+const sqlite = require('node:sqlite');
+const { DatabaseSync } = require('node:sqlite');
+const database = new DatabaseSync('./storage/votes.sqlite');
+
+const ifExist = database.prepare(`SELECT name FROM sqlite_master WHERE type='table' AND name='votes'`);
+if (ifExist.all().length == 0) {
+database.exec(`
+  CREATE TABLE votes(
+  vote INTEGER,
+  time INTEGER
+   ) STRICT
+  
+  `)
+
+}
+const insert = database.prepare('INSERT INTO votes (vote, time) VALUES (?, ?)');
+const getVotes = database.prepare('SELECT vote, time FROM votes');
+const count = database.prepare('SELECT COUNT(vote) as count FROM votes WHERE vote = ?')
+
+
+////// server
+app.get('/all', (req, res) => {
+  res.send(`Vote1 total votes: ${JSON.stringify(getVotes.all())}`)
+})
+
+app.get('/Vote1', (req, res) => {
+  res.send(`Vote1 total votes: ${count.get(1).count}`)
+})
+
+app.get('/Vote2', (req, res) => {
+  res.send(`Vote2 total votes: ${count.get(2).count}`)
+})
+
+app.get('/Vote3', (req, res) => {
+  res.send(`Vote3 total votes: ${count.get(3).count}`)
+})
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<----------POST----------------------->>>>>>>>>>>>>>>>>>>>>>>>>>
+app.post('/Vote1', (req, res) => {
+  insert.run(1, Date.now())
+  res.send(`Added vote at vote1. Total votes: ${count.get(1).count} `)
+})
+
+app.post('/Vote2', (req, res) => {
+ insert.run(2, Date.now())
+  res.send(`Added vote at vote2. Total votes: ${count.get(2).count}`)
+})
+
+
+app.post('/Vote3', (req, res) => {
+  insert.run(3, Date.now())
+  res.send(`Added vote at vote3. Total votes: ${count.get(3).count}`)
+})
+
+
+
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
